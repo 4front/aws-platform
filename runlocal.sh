166e981b0beff0ddd626afff1f9d5bdf4bd0d52e
David Von Lehman <david.vonlehman@nordstrom.com>
2015-07-02 10:52:14 -0700

    Just install add-ons in the main node_modules dir

--- runlocal.sh
+++ runlocal.sh
@@ -7,11 +7,7 @@ export FF_JWT_TOKEN_SECRET='token-secret'
 export FF_DEPLOYED_ASSETS_PATH='/deployments'
 export FF_S3_DEPLOYMENTS_BUCKET='fake-s3-bucket'
 export FF_SESSION_SECRET='session-secret'
-export FF_LDAP_URL='ldap://LDAP0319.nordstrom.net'
-export FF_LDAP_BASE_DN='dc=nordstrom,dc=net'
 export DEBUG="4front:*"
-export FF_ADDONS="lodash"
-export FF_INSTALL_ADDONS="0"
 
 echo $1
 
