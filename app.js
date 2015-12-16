67fe8b19077cc72d89d541f18c6a2bf64a44d5da
David Von Lehman <david.vonlehman@gmail.com>
2015-12-16 14:19:05 -0800

    Change app initialization to synchronous

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var http = require('http');
 var debug = require('debug')('4front:aws-platform');
 var _ = require('lodash');
 var serveStatic = require('serve-static');
@@ -11,98 +10,68 @@ var app = express();
 app.enable('trust proxy');
 app.set('view engine', 'jade');
 
-function initializationError(err) {
-  (app.settings.logger ? app.settings.logger : console).error('App configuration error: %s', err.stack);
-  return process.exit();
-}
+require('./lib/configure')(app);
 
-require('./lib/configure')(app, function(configErr) {
-  if (configErr) {
-    return initializationError(configErr);
-  }
+app.use(app.settings.logger.middleware.request);
 
-  try {
-    registerRoutes();
-  } catch (routeError) {
-    return initializationError(routeError);
-  }
+// No harm in parsing cookies on all requests. But intentionally
+// not parsing the body as the express-request-proxy needs the
+// raw body to passthrough. The body-parser can be declared at the
+// level in the middleware stack where it is needed.
+app.use(require('cookie-parser')());
 
-  startExpressServer();
+// Initialize the extended request object
+app.use(function(req, res, next) {
+  req.ext = {};
+  next();
 });
 
-function registerRoutes() {
-  debug('register routes');
-  app.use(app.settings.logger.middleware.request);
+// The appHostRouter and apiRouter are also needed by the health check.
+// However the real routes cannot be mounted until after the __health endpoint.
+// Otherwise the localhost:8080 that the ELB makes the call on will result in
+// a 404 response from the apphost which will cause the servers to be deemed
+// unhealthy and taken out of service.
+var appHostRouter = require('4front-apphost')(app.settings);
+var apiRouter = require('4front-api')(app.settings);
 
-  // No harm in parsing cookies on all requests. But intentionally
-  // not parsing the body as the express-request-proxy needs the
-  // raw body to passthrough. The body-parser can be declared at the
-  // level in the middleware stack where it is needed.
-  app.use(require('cookie-parser')());
+app.use('/__debug', shared.routes.debug(app.settings));
+app.use('/__health', shared.routes.healthCheck(app.settings, apiRouter, appHostRouter));
 
-  // Initialize the extended request object
-  app.use(function(req, res, next) {
-    req.ext = {};
-    next();
-  });
+// Mount the apphosting router
+app.use(appHostRouter);
 
-  // The appHostRouter and apiRouter are also needed by the health check.
-  // However the real routes cannot be mounted until after the __health endpoint.
-  // Otherwise the localhost:8080 that the ELB makes the call on will result in
-  // a 404 response from the apphost which will cause the servers to be deemed
-  // unhealthy and taken out of service.
-  var appHostRouter = require('4front-apphost')(app.settings);
-  var apiRouter = require('4front-api')(app.settings);
+// Mount the api router
+app.use('/api', apiRouter);
 
-  app.use('/__debug', shared.routes.debug(app.settings));
-  app.use('/__health', shared.routes.healthCheck(app.settings, apiRouter, appHostRouter));
-
-  // Mount the apphosting router
-  app.use(appHostRouter);
-
-  // Mount the api router
-  app.use('/api', apiRouter);
-
-  var portal;
-  // For ease of development on portal
-  if (process.env.FF_DEV_LOCAL_PORTAL) {
-    debug('using local filesystem portal');
-    portal = require('../../4front/portal');
-  } else {
-    portal = require('4front-portal');
-  }
-
-  app.use('/portal', portal(_.extend({}, app.settings, {
-    basePath: '/portal',
-    apiUrl: '/api'
-  })));
-
-  // Deliberately register the static middleware after all the other routes
-  app.use(serveStatic('public/', {fallthrough: true, index: false}));
-
-  app.get('/', function(req, res, next) {
-    debug('root request');
-    if (req.hostname !== app.settings.virtualHost) return next();
-
-    res.redirect('/portal');
-  });
-
-  app.use(shared.routes.catchAll(app.settings));
-
-  app.use(shared.routes.error(app.settings));
+var portal;
+// For ease of development on portal
+if (process.env.FF_DEV_LOCAL_PORTAL) {
+  debug('using local filesystem portal');
+  portal = require('../../4front/portal');
+} else {
+  portal = require('4front-portal');
 }
 
-// Start the express server
-// Assuming that SSL cert is terminated upstream by something like Apache, Ngninx, or ELB,
-// so the node app only needs to listen over http.
-function startExpressServer() {
-  debug('start the express server');
-  var server = http.createServer(app);
-  server.listen(app.settings.port, function(err) {
-    if (err) {
-      return initializationError(err);
-    }
+app.use('/portal', portal(_.extend({}, app.settings, {
+  basePath: '/portal',
+  apiUrl: '/api'
+})));
 
-    app.settings.logger.info('4front platform running on port ' + app.settings.port);
-  });
-}
+// Deliberately register the static middleware after all the other routes
+app.use(serveStatic('public/', {fallthrough: true, index: false}));
+
+app.get('/', function(req, res, next) {
+  debug('root request');
+  if (req.hostname !== app.settings.virtualHost) return next();
+
+  res.redirect('/portal');
+});
+
+app.use(shared.routes.catchAll(app.settings));
+
+app.use(shared.routes.error(app.settings));
+
+debug('start the express server');
+app.listen(app.settings.port, function() {
+  debug('server listening on port ' + app.settings.port);
+});
--- runlocal.sh
+++ runlocal.sh
@@ -2,8 +2,13 @@
 # source ./localenv.sh
 export FF_S3_BUCKET="fake-s3-bucket"
 export DEBUG="4front:*,express-request-proxy,express-session,s3-proxy"
+export FAKE_S3_PORT=4658
+export DYNAMO_LOCAL_PORT=8000
 
 echo $1
+
+# Start the fakeS3 server
+./node_modules/.bin/s3rver --port $FAKE_S3_PORT -d ./ -s &
 
 if [ $1 = "debug" ]
 then
